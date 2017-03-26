05c0045cd359ec16fddb104bf8e602c9007dbd96
jordanwade <jjwade31@gmail.com>
2017-03-26 15:03:06 -0400

    Add Inventory, updateResource, removeResource methods

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component }from 'react';
 import AddResourceForm from  './AddResourceForm';
 import Header from  './Header';
 import Resource from  './Resource';
+import Inventory from  './Inventory';
 import base from '../base'; 
 
 class App extends Component {
@@ -10,6 +11,8 @@ class App extends Component {
     super();
 
     this.addResource = this.addResource.bind(this);
+    this.updateResource = this.updateResource.bind(this);
+    this.removeResource = this.removeResource.bind(this);
 
     // getintial state
     this.state = {
@@ -39,6 +42,18 @@ class App extends Component {
     this.setState({ resources });
   }
 
+  updateResource(key, updatedResource) {
+    const resources = {...this.state.resources};
+    resources[key] = updatedResource;
+    this.setState({ resources });
+  }
+
+  removeResource(key) {
+    const resources = {...this.state.resources};
+    resources[key] = null;
+    this.setState({ resources });
+  }
+
   render() {
     return (
       <div className="App">
@@ -50,6 +65,11 @@ class App extends Component {
             .map(key => <Resource key={key} index={key} details={this.state.resources[key]} />)
           }
         </ul>
+        <Inventory
+          resources={this.state.resources}
+          updateResource={this.updateResource}
+          removeResource={this.removeResource}
+        />
       </div>
     );
   }
