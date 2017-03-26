24667b322fb97779263a864692ff2538903160a9
jordanwade <jjwade31@gmail.com>
2017-03-26 15:02:49 -0400

    Add inventory component

---
+++ src/components/Inventory.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import AddResourceForm from './AddResourceForm';
+
+class Inventory extends React.Component {
+  constructor() {
+    super();
+    this.renderInventory = this.renderInventory.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e, key) {
+    const resource = this.props.resources[key]; 
+    // take a copy of that resource and update if with the new data
+    const updatedResource = {
+      ...resource,
+      [e.target.name]:  e.target.value
+    }
+    this.props.updateResource(key, updatedResource)
+  }
+
+  renderInventory(key) {
+    const resource = this.props.resources[key]; 
+    return (
+      <div className="resource-edit" key={key}>
+        <input name="name" type="text" value={resource.name}  placeholder="Resource Name" onChange={(e) => this.handleChange(e, key)}/>
+        <input name="url"  type="text" value={resource.url}  placeholder="Resource url" onChange={(e) => this.handleChange(e, key)}/>
+        <textarea name="desc" value={resource.desc}  placeholder="Resource desc" onChange={(e) => this.handleChange(e, key)}></textarea>
+        <textarea name="imple" value={resource.imple}  placeholder="Resource implementation" onChange={(e) => this.handleChange(e, key)}></textarea>
+        <button onClick={() => this.props.removeResource(key)}>Remove Resource</button>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Inventory</h2>
+        {Object.keys(this.props.resources).map(this.renderInventory)}
+      </div>  
+    )
+  }
+}
+
+export default Inventory
