854d973801b1e48cd0b5aca86db1fe8f1465f23e
jordanwade <jjwade31@gmail.com>
2017-03-27 15:12:30 -0400

    Add new `ResourceShow` component

---
+++ src/components/ResourceShow.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import base from '../base';
+
+class ResourceShow extends Component {
+
+  constructor() {
+    super();
+
+    // getintial state
+    this.state = {
+      resources: {},
+    };
+  }
+
+  componentWillMount() {
+    this.ref = base.syncState(`resources/${this.props.params.resourceId}`
+      , {
+        context: this,
+        state: 'resources'
+    })
+    this.setState();
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
+  render() { 
+
+    const resource = this.state.resources;
+
+    return (
+      <div>
+        <h3 className="resource-name">
+          {resource.name}
+        </h3>
+        <span className="url">{resource.url}</span>
+        <p>{resource.desc}</p>
+        <p>{resource.imple}</p>
+      </div> 
+    )
+  }
+}
+
+ResourceShow.propTypes = {
+  params: React.PropTypes.object.isRequired
+}
+
+export default ResourceShow
