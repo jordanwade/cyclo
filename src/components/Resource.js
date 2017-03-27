f3387c54e5f4fcd1d1a1dee3c55eba004277952a
jordanwade <jjwade31@gmail.com>
2017-03-27 15:11:12 -0400

    Update Resource component and add proptypes

--- src/components/Resource.js
+++ src/components/Resource.js
@@ -1,23 +1,36 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
 
 class Resource extends Component {
+
+  goToResource() {
+    const resourceId = this.props.index;
+    this.context.router.transitionTo(`/resource/${resourceId}`);
+  }
+
   render() { 
     const { details, index } = this.props;
-    const isAvailable = details.status === 'available';
-    const buttonText = isAvailable ? 'Add To Order' : 'Sold Out!';
 
     return (
-      <li className="menu-fish">
-        <h3 className="fish-name">
+      <li>
+        <h3 className="resource-name">
           {details.name}
         </h3>
-        <span className="price">{details.url}</span>
+        <span className="url">{details.url}</span>
+        <span className="technology">{details.technology}</span>
         <p>{details.desc}</p>
         <p>{details.imple}</p>
-        
+        <button onClick={(e) => this.goToResource(e)} >Visit Resource →</button>
       </li> 
     )
   }
+}
+
+Resource.contextTypes = {
+   router: React.PropTypes.object
+}
+
+Resource.propTypes = {
+  index: React.PropTypes.string.isRequired
 }
 
 export default Resource
