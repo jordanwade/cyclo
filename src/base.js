06907d0a50d65cb3202953f22aefbbc8d3f01985
jordanwade <jjwade31@gmail.com>
2017-03-29 14:42:11 -0400

    Sync state with Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDkX8z0YO2rE6jwgZGULYFuC2HZjgAC6lI",
+    authDomain: "cyclo-d8612.firebaseapp.com",
+    databaseURL: "https://cyclo-d8612.firebaseio.com"
+});
+
+export default base;
