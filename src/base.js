c7669bdbb2cd09912fe5c2b2d872048bc0a4ae34
jordanwade <jjwade31@gmail.com>
2017-05-07 14:31:29 -0400

    refactor: firebase setup

--- src/base.js
+++ src/base.js
@@ -1,11 +1,17 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase';
 
-const base = {
+const config = {
     apiKey: "AIzaSyDkX8z0YO2rE6jwgZGULYFuC2HZjgAC6lI",
     authDomain: "cyclo-d8612.firebaseapp.com",
     databaseURL: "https://cyclo-d8612.firebaseio.com"
 };
 
-firebase.initializeApp(base);
+firebase.initializeApp(config);
 
-export default base;
+export default firebase;
+
+export const database = firebase.database();
+export const auth = firebase.auth();
+export const gitAuthProvider = new firebase.auth.GithubAuthProvider();
+
+
